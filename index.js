7cd90262b1624a562c2e9ef2a2ee19a1e431c24c
Leonardo Campos <108032433+LeonardoCampos777@users.noreply.github.com>
2023-03-18 23:59:24 -0300

    update

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('JavaScript Funcionando!')
+
+let Firstname = 'Leonardo';
+let Lastname = 'Campos';
+
+console.log(Firstname);
+console.log(Lastname);
+
+let corCarro ='Azul marinho';
+let idade = '24';
+let namorada ='Rayanny Kataprima';
+
+console.log(corCarro);
+console.log(idade);
+console.log(namorada);
+
+
+let meuPai ='Luiz Ferreira Campos';
+let minhaMae ='Edileuza Maria de Souza';
+let irma ='Palloma Souza Campos';
+let sobrinho ='Ravi Muniz';
+
+console.log(meuPai);
+console.log(minhaMae);
+console.log(irma);
+console.log(sobrinho);
+
+
+const price = 30;
+// price = 20;
+console.log (price);
