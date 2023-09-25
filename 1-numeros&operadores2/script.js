4eda6adc7bcad3a947c2a441163cdb7025559919
Leonardo Campos <leonardo.kiss66@live.com>
2023-09-25 20:44:10 -0300

    update

---
+++ 1-numeros&operadores2/script.js
@@ -0,0 +1,21 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20;
+console.log(total); // 35 //
+// Crie duas expressões que retornem NaN
+var nan1 = +"24s";
+var nan2 = +"2023a";
+console.log(nan1);
+console.log(nan2);
+// Somar a string '200' com o número 50 e retornar 250
+var stringNumero = +"200" + 50;
+console.log(stringNumero);
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+console.log(x++);
+console.log(x);
+// Como dividir o peso por 2?
+var numero = +"80" / 2;
+var unidade = "kg";
+var peso = numero + unidade; // '80kg'
+
+console.log(peso);
---
+++ 2-boolean&condicionais/scritp.js
@@ -0,0 +1,77 @@
+const lindo = false;
+const gostosao = true;
+
+if(lindo){
+  console.log("eu sou lindo");
+ }
+ else if(gostosao){
+  console.log("isso mesmo sou gostoso caralho");
+} else{
+  console.log("mentira não sou lindo e nem gostoso :(")
+}
+
+const nome = "10kg" / 10;
+
+if(nome){
+  console.log(nome);
+} else{
+  console.log('isso é false caralho');
+}
+
+if(!lindo){
+  console.log("agora peguei o macete peguei o macete")
+}
+
+if (!!gostosao){
+  console.log(gostosao);
+}
+
+if (!!nome){
+  console.log(nome);
+} else{
+  console.log("mais uma vez o o sinal !! mostrou que é false");
+}
+
+if(!!lindo){
+  console.log(lindo);
+} else{
+  console.log("mais uma vez o o sinal !! mostrou que é false");
+}
+
+const condicional1 = (5 - 5) && (10 + 10);
+if(condicional1){
+  console.log('Verdadeiro calica');
+} else{
+  console.log('Errouu calica é false papai');
+}
+
+const condicional2 = (5 - 10) && (10 + 10);
+if(condicional2){
+  console.log('Verdadeiro calica chupa que é de uva');
+} else{
+  console.log('Errouu calica é false papai');
+}
+
+const condicional3 = (5 - 5) || (5 + 10);
+if(condicional3){
+  console.log("diz pra mim oque é ctz que é true");
+} else{
+  console.log('iii menozinn tu errou é false kakaka');
+}
+
+
+const corPreferida = "roxo";
+
+switch (corPreferida) {
+  case 'Vermelho':
+    console.log('caralho sou o homem aranha');
+    break;
+  case 'Verde':
+    console.log('caralho sou o Hulk mané');
+    break;
+  case 'Azul':
+    console.log('caralho sou o capiton americon');
+    break;
+  default:
+    console.log('você não é porra nenhuma, vá cuidar em estudar vagabundo');      
+}
