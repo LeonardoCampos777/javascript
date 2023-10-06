a7ee510552bfd3e913e08dd5b2712bb65d6d3a91
Leonardo Campos <leonardo.kiss66@live.com>
2023-10-06 20:22:04 -0300

    update

--- 5-arrays&loops/script.js
+++ 5-arrays&loops/script.js
@@ -1,8 +1,8 @@
-const videoGames = ["Nitendo", "PS2", "Computador"];
+const videoGames = ["Nitendo", "PS2", "Xbox", "SuperNitendo"];
 
-const ultimoItem = videoGames.pop();
+//const ultimoItem = videoGames.pop();
 
-videoGames.push('Videogame que tinha a pistola');
+//videoGames.push('Videogame que tinha a pistola');
 
 console.log(videoGames);
 
@@ -13,3 +13,27 @@ for(var numero = 1; numero <= 10; numero++){
 //numero ++ é igual a numero = numero + 1
 //numero é igual a numero + 1 sempre
 //O operador ++ é dito de incremento, ele aumenta o valor da variável em uma unidade.
+
+const videoGames2 = ["Nitendo", "PS2", "Xbox", "SuperNitendo"];
+for (let item = 0; item < videoGames.length; item++){
+  console.log(videoGames[item]);
+  if(videoGames[item] === "PS2"){
+    break;
+  }
+}
+//videoGames.length; é o total de itens que tem dentro dessa Array.
+
+const jogadores = ["Ronaldinho", "Neymar", "Ronaldo", "Ibrahimović", "Káká"];
+
+jogadores.forEach(function(jogador,index,array){
+  console.log(jogador,index,array);
+});
+
+var numero = 0;
+var maximo = 50;
+for(;numero < maximo;) {
+  console.log(numero);
+  numero++;
+}
+
+
