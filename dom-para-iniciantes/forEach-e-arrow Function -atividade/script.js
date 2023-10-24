ecf722554370891ceccae8b30e240196a6c1ede9
Leonardo Campos <leonardo.kiss66@live.com>
2023-10-23 23:06:33 -0300

    update

---
+++ dom-para-iniciantes/forEach-e-arrow Function -atividade/script.js
@@ -0,0 +1,27 @@
+// Mostre no console cada parágrado do site
+const paragrafos = document.querySelectorAll('p');
+
+paragrafos.forEach(function(paragrafo){
+  //console.log(paragrafo);
+});
+
+// Mostre o texto dos parágrafos no console
+const textosParagrafos = document.querySelectorAll('p');
+
+textosParagrafos.forEach(function(paragrafo){
+  //console.log(paragrafo.innerText);
+});
+
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => console.log(i++));
--- dom-para-iniciantes/forEach-e-arrow Function/script.js
+++ dom-para-iniciantes/forEach-e-arrow Function/script.js
@@ -1,1 +1,33 @@
-const imgs = document.querySelectorAll('imgs');
+//const imgs = document.querySelectorAll('img');
+
+
+//imgs.forEach((item) =>{ // aqui a gente vê uma Arrow Function, passamos o argumento nas aspas () e a função digitamos o arco =>
+  //console.log(item);
+//});
+
+
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
+
+titulosArray.forEach(function(item){
+  //console.log(item);
+});
+
+//const imgs = document.querySelectorAll('img');
+
+
+//imgs.forEach(item =>{
+ // console.log(item);
+//});
+// você pode também usar se tiver apenas um argumento sem os parenteses () passar o item sem os pareteses.
+// porem não é aconselhavel, portanto vamos sempre colocar os parenteses sempre 
+
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(item => console.log(item));
+
+// se você tiver um código que só tem apenas uma linha, você pode tirar as chaves {}, e abreviar mais ainda
+// utilizando o código apenas em uma só linha, destá forma.
+// não coloque o ponto e virgula ; no fim do console.log(). lembre-se disso.
+// e tabém pode ter casos de não ter argumento nenhum.
