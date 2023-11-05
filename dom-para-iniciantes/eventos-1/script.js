2eadd5ae8c3aece0b11a1f8f31bbf5087143221c
Leonardo Campos <leonardo.kiss66@live.com>
2023-11-05 00:10:47 -0300

    update

---
+++ dom-para-iniciantes/eventos-1/script.js
@@ -0,0 +1,53 @@
+const img = document.querySelector('img');
+
+
+function callback(event){
+  console.log(event);
+}
+
+//img.addEventListener('click', callback);
+
+const animaisLista = document.querySelector('.animais-lista');
+
+
+function callbackLista(event){
+  console.log(event.currentTarget);
+  console.log(event.target);
+  console.log(event.type);
+  console.log(event.path);
+}
+
+animaisLista.addEventListener('click', callbackLista);
+
+const linkExterno = document.querySelector('a[href^="https"]');
+
+function handleLinkExterno(event){
+  event.preventDefault();
+  console.log(this.getAttribute('href'));
+  console.log(event.currentTarget);
+};
+
+linkExterno.addEventListener('click', handleLinkExterno);
+
+const h1 = document.querySelector('h1');
+
+function handleEvent(event){
+  console.log(event.type, event);
+};
+
+//h1.addEventListener('click', handleEvent);
+//h1.addEventListener('mouseenter', handleEvent);
+//h1.addEventListener('mousemove', handleEvent);
+
+//window.addEventListener('scroll', handleEvent);
+//window.addEventListener('resize', handleEvent);
+//window.addEventListener('keydown', handleEvent);
+
+function handleKeyBoard(event){
+ console.log(event.key);
+ if(event.key === 'l'){
+  document.body.classList.toggle('azul');
+ }
+}
+
+window.addEventListener('keydown', handleKeyBoard);
