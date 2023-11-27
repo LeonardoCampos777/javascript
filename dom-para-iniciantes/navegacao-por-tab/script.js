c73dcd39fc9b97738044d0df4a9a541e2f3ae0b8
Leonardo Campos <leonardo.kiss66@live.com>
2023-11-27 08:19:26 -0300

    update

--- dom-para-iniciantes/navegacao-por-tab/script.js
+++ dom-para-iniciantes/navegacao-por-tab/script.js
@@ -1,25 +1,37 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if(tabMenu.length && tabContent.length){
-  tabContent[0].classList.add('ativo');
-  
-  function activeTab(index){
-    tabContent.forEach((section) =>{
-      section.classList.remove('ativo');
-    })
-    tabContent[index].classList.add('ativo');
-  }
-  
-  tabMenu.forEach((itemMenu, index) =>{
-    itemMenu.addEventListener('click', () =>{
-      activeTab(index);
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    };
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-  });
-  
+  };
 }
+initTabNav();
 
 
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion(){
+  this.classList.toggle('ativo');
+  this.nextElementSibling.classList.toggle('ativo');
+};
+
+accordionList.forEach((item) =>{
+  item.addEventListener('click', activeAccordion);
+});
 
 // UMA NAVEGAÇÃO POR TAB É UMA NAVEGAÇÃO RELACIONADA COM UMA LISTA DE ITENS  E OUTRA LISTA DE ITENS
 // E COM ISSO AO CLICAR EM UMA LISTA, VAI ATIVAR A OUTRA!!
@@ -31,6 +43,6 @@ if(tabMenu.length && tabContent.length){
 // ai temos o itemMenu, que é cada li de dentro desse tabMenu
 // e temos o index, que vai retornar o número index, de cada imagem clicada da lista  exemplo: 0/1/2/3/4/5/6.
 // em cada item eu vou adicionar o evento de click, e ao clicar vai executar a funcão e me mostrar o número do index e com isso
-// ativar a funcão o activeTab lá de cima. 
+// ativar a funcão o activeTab lá de cima.
 
 // QUANDO EU CLICAR EU QUER OQUE PASSE O NÚMERO DO INDEX DENTRO DA MINHA FUNÇÃO ACTIVE TAB!!!
